e6dc113e1898a417659e66c13399a2f1b7c28caf
Alejandro Vargas Garran <avargaga@emeal.nttdata.com>
2025-06-06 10:14:30 +0200

    TRI-001 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home }
+    /* { path: 'multiple-options', component: MultipleOptionsComponent },
+    { path: 'single-option', component: SingleOptionsComponent }, */
+];
